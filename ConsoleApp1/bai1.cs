9a6db5e7a5fd719088e1881a8c7883d0607723bf
Đức Bảo <ndbpro99@gmail.com>
2020-11-05 19:55:21 +0700

    Add project files.

---
+++ ConsoleApp1/bai1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+   
+    class TienDien
+
+    {
+        private string chuho;
+        private string diachi;
+        private int SDTN, SDTT;
+        public TienDien()
+        {
+            chuho = diachi = "";
+            SDTT = SDTN;
+        }
+        public TienDien(string chuho, string diachi, int SDTT, int SDTN)
+        {
+            this.chuho = chuho;
+            this.diachi = diachi;
+            this.SDTN = SDTN;
+            this.SDTT = SDTT;
+        }
+        public void nhap()
+        {           
+            Console.WriteLine("nhap ten chu ho");
+            chuho = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("nhap dia chi");
+            diachi = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("nhap so dien thang truoc");
+            SDTT = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap so dien thang nay");
+            SDTN = int.Parse(Console.ReadLine());
+
+
+        }
+        public void Hien()
+        {
+            Console.WriteLine("thông tin hiển thị");
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}", chuho, diachi, SDTN, SDTT);
+            Console.WriteLine();
+        }
+        public int sodiendadung
+        {
+            get
+            {
+                return (SDTN - SDTT);
+
+            }
+
+        }
+        public int tiendien()
+        {
+            return sodiendadung * 500;
+        }
+
+    }
+    class TienDienMoi: TienDien
+    {
+        private int dinhmuc;
+        public TienDienMoi() : base()
+        {
+            dinhmuc = 50;
+        }
+        public TienDienMoi(string tenchuho, string diachi, int SDTT, int SDTN, int dinhmuc) : base(tenchuho, diachi, SDTT, SDTN)
+        {
+            this.dinhmuc = dinhmuc;
+        }
+        public new void nhap()
+        {
+            base.nhap();
+            Console.Write("Nhap he so dinh muc: ");
+            dinhmuc = int.Parse(Console.ReadLine());
+
+        }
+        public new int tiendien()
+        {
+            if (sodiendadung < dinhmuc) return sodiendadung * 500;
+            else return sodiendadung * 600;
+        }
+    }
+    class Test1
+    {
+        static void Main1(string[] args)
+        {
+            TienDienMoi t = new TienDienMoi();
+            t.nhap();
+            t.Hien();
+            Console.WriteLine("Tien dien phai tra:{0}", t.tiendien());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/bai2/bai2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.NewFolder
+{  
+    class TamGiac
+    {
+        private double A, B, C;
+
+        public TamGiac()
+        {
+            A = B = C = 0;
+        }
+        public TamGiac(double canhA, double canhB, double canhC)
+        {
+            this.A = canhA;
+            this.B = canhB;
+            this.C = canhC;
+        }
+        public void nhap()
+        {
+            Console.Write(" nhap do dai canh A:"); A = int.Parse(Console.ReadLine());
+            Console.Write(" nhap do dai canh B:"); B = int.Parse(Console.ReadLine());
+            Console.Write(" nhap do dai canh C:"); C = int.Parse(Console.ReadLine());
+        }
+        public void hien()
+        {
+            Console.WriteLine("thông tin về hình ......");
+            Console.WriteLine("Canh A = {0}\n\n Canh B = {1}\n\n Canh C = {2}", A, B, C);
+            Console.WriteLine("dien tich day = {0}", TinhDienTich);
+        }
+        public bool kiemtra(double A, double B, double C)
+        {
+            return A + B > C && A + C > B && B + C > A;
+        }
+        public double TinhDienTich
+        {
+            get
+            {
+                if (kiemtra(A, B, C))
+                {
+                    double p = A + B + C / 2;
+                    double s = Math.Sqrt(p * ((p - A) * (p - B) * (p - C)));
+                    return s;
+                }
+                else
+                {
+                    Console.WriteLine("ba diem thang hang");
+                }
+                return TinhDienTich;
+            }
+
+        }
+    }
+    class TuDien : TamGiac
+    {
+        private double h;
+        public TuDien()
+        {
+            h = 0;
+        }
+        public TuDien(double A, double B, double C, double h)
+        {
+            this.h = h;
+        }
+        public new void nhap()
+        {
+            base.nhap();
+            Console.Write("nhap chieu cao :"); h = int.Parse(Console.ReadLine());
+        }
+        public new void hien()
+        {
+            base.hien();
+            Console.WriteLine("chieu cao cua tu dien: " + h);
+            Console.WriteLine("dien tich tu dien =" + tinhdienticTD());
+        }
+        public double tinhdienticTD()
+        {
+            double j = 1 / 3 * h * TinhDienTich;
+            return j;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TamGiac t = new TamGiac();
+            t.nhap();
+            t.hien();
+            Console.ReadKey();
+            TuDien g = new TuDien();
+            g.nhap();
+            g.hien();
+
+
+        }
+    }
+}
